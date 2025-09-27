4e40f946335afeafa06457494546be739dc8482f
plasma4 <72562754+plasma4@users.noreply.github.com>
2025-09-27 11:54:53 -0500

    Key logic/sprite

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const game = {
     funcs: {},
     canvas: canvas,
     ctx: ctx,
+    sheets: {
+        main: "assets/main.png",
+    },
+    levelData: [
+        [
+            { type: 1234 }
+        ]
+    ],
     consts: {
         DOWN: 0,
         UP: 1
@@ -25,12 +33,41 @@ funcs.handleKey = function (type, { key, repeat, shiftKey, metaKey, ctrlKey }) {
             // ignore
         } else {
             // do stuff...
-            keysHeld = keysHeld.push(key)
-            keys
+            if (held) keysHeld.push(key)
+            if (keyData.shift) {
+                keysHeld.push("shift")
+            }
+            if (keyData.ctrl) {
+                keysHeld.push("ctrl")
+            }
         }
     } else if (type === C.UP) {
-        keyData
+        // do stuff...
+        if (held) keysHeld.splice(key, 1)
+        if (keyData.shift)  funcs.addKey("shift")
+        if (keyData.ctrl) {
+            keysHeld.push("ctrl")
+        }
     }
+}
+funcs.heldKey = keysHeld.includes.bind(keysHeld)
+funcs.addKey = function() {
+    
+}
+
+function renderSprite(x, y, rotation, sheet) {
+    ctx.save()
+    ctx.translate(x, y)
+    if (rotation !== 0) ctx.rotate(rotation)
+    const sw = sheet.spriteW, sh = sheet.spriteH
+    const cols = sheet.imageW / sw
+    const sx = (index % cols) * sw
+    const sy = Math.floor(index / cols) * sh
+    const dw = sw
+    const dh = sh
+    ctx.imageSmoothingEnabled = false
+    ctx.drawImage(sheet.image, sx, sy, sw, sh, -dw * 0.5, -dh * 0.5, dw, dh) // TODO
+    ctx.restore()
 }
 
 document.addEventListener("resize", game.handleResize)
