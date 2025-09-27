6c30a46c8b5bbe6a12be92a951fb871cd603c4ac
Jmeow <j@jmeow.net>
2025-09-27 14:21:06 -0500

    Add about screen

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const player = {
     oldX: 0,
     oldY: 0,
     px: 0,
-    py: 0
+    py: 0,
 }
 const game = {
     w: 1600,
@@ -25,16 +25,17 @@ const game = {
     currentSprites: [],
     sheets: {
         main: {
-            src: './static/assets/sprites/main.png',
+            src: "./static/assets/sprites/main.png",
             spriteW: 24,
             spriteH: 24,
             imageW: 96,
             imageH: 24,
             ids: ["player", "coin", "!", "?"],
-        }
+        },
     },
     levelData: [
-        { // Note that each level in levelData gains a new property `data` containing an array of sprites.
+        {
+            // Note that each level in levelData gains a new property `data` containing an array of sprites.
             name: "Main Level",
             keys: { "P": "main/player" }, // These are the keys that convert a character in the map to a sprite; sprite interactions are handled in spriteInteraction
             items: [{ type: "P", x: 0, y: 0 }], // Additional items that might need to be added to the object. This, for example, can used to add objects in partial coordinates.
@@ -43,28 +44,21 @@ const game = {
                 // obj.
                 return obj
             },
-            map:
-                `P
-
-_`
-        }
+            map: `asdfasdfsadf`,
+        },
     ],
     consts: {
         DOWN: 0,
-        UP: 1
-    }
+        UP: 1,
+    },
 }
 var data = game.data
 var funcs = game.funcs
 const C = game.consts
 
-
-
 funcs.spriteInteraction = function (sprite) {
     var interactions = {
         intersecting: funcs.isIntersecting(sprite),
-        over: funcs.wentOver(sprite),
-        out: funcs.wentOut(sprite)
     }
     funcs.renderSprite("player", "main", 0, 0)
 }
@@ -82,7 +76,8 @@ funcs.update = function () {
     game.keysUp.length = 0
     if (funcs.isDown("Escape")) {
         canvas.style.display = "none"
-        document.getElementById("menu").removeAttribute("style")
+        document.getElementById("menu").style.display = "block"
+        document.getElementById("about").style.display = "none"
     } else {
         setTimeout(funcs.update, frame % 3 === 0 ? 34 : 33)
     }
@@ -93,6 +88,14 @@ document.addEventListener("DOMContentLoaded", function () {
     funcs.addClickEvent("playButton", () => {
         document.getElementById("menu").style.display = "none"
         canvas.removeAttribute("style")
+    })
+    funcs.addClickEvent("aboutButton", () => {
+        document.getElementById("menu").style.display = "none"
+        document.getElementById("about").style.display = "block"
+    })
+    funcs.addClickEvent("aboutBackButton", () => {
+        document.getElementById("menu").style.display = "block"
+        document.getElementById("about").style.display = "none"
     })
 })
 
