85719319dc054d2e95ff8349c028d22828c9c08a
swaritnarang <narangswarit02@gmail.com>
2025-09-27 13:30:26 -0500

    add button click handling

---
+++ src/buttonHandler.js
@@ -0,0 +1,7 @@
+const playButton = document.getElementById("playButton");   
+
+function playButtonClicked() {
+    console.log("test");
+}
+
+playButton.addEventListener("click", playButtonClicked);
