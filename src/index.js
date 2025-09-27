5320c841f72aa9cb675f1e0e65c98c818c98587a
plasma4 <72562754+plasma4@users.noreply.github.com>
2025-09-27 12:08:15 -0500

    More keys and sprite rendering

--- src/index.js
+++ src/index.js
@@ -3,22 +3,31 @@ const canvas = document.getElementsByTagName("canvas")[0]
 const ctx = canvas.getContext("2d")
 const game = {
     keysHeld: [],
+    keysDown: [],
+    keysUp: [],
     mouse: false, // or [x, y]
     data: {},
     funcs: {},
     canvas: canvas,
     ctx: ctx,
     sheets: {
-        main: "assets/main.png",
-    },
-    levelData: [
-        [
-            { type: 1234 }
-        ]
-    ],
-    consts: {
-        DOWN: 0,
-        UP: 1
+        main: {
+            src: 'assets/main.png',
+            spriteW: 32,
+            spriteH: 16,
+            imageW: 16,
+            imageH: 16,
+            ids: ["player", "coin", "!", "?"]
+        },
+        levelData: [
+            [
+                { type: 1234 }
+            ]
+        ],
+        consts: {
+            DOWN: 0,
+            UP: 1
+        }
     }
 }
 
@@ -32,41 +41,62 @@ funcs.handleKey = function (type, { key, repeat, shiftKey, metaKey, ctrlKey }) {
         if (keyData.repeat) {
             // ignore
         } else {
-            // do stuff...
-            if (held) keysHeld.push(key)
-            if (keyData.shift) {
-                keysHeld.push("shift")
-            }
-            if (keyData.ctrl) {
-                keysHeld.push("ctrl")
-            }
+            if (held) funcs.addKey(key)
+            if (keyData.shift) funcs.addKey("shift")
+            if (keyData.ctrl) funcs.addKey("ctrl")
         }
     } else if (type === C.UP) {
-        // do stuff...
         if (held) keysHeld.splice(key, 1)
-        if (keyData.shift)  funcs.addKey("shift")
-        if (keyData.ctrl) {
-            keysHeld.push("ctrl")
-        }
+        if (keyData.shift) funcs.removeKey("shift")
+        if (keyData.ctrl) funcs.removeKey("ctrl")
     }
 }
 funcs.heldKey = keysHeld.includes.bind(keysHeld)
-funcs.addKey = function() {
-    
+funcs.addKey = function (key) {
+    if (!keysDown.includes(key)) {
+        keysDown.push(key)
+    }
+    if (!keysHeld.includes(key)) {
+        keysHeld.push(key)
+        return true
+    }
+    return false
+}
+funcs.removeKey = function (key) {
+    if (!keysUp.includes(key)) {
+        keysUp.push(key)
+    }
+    var index = keysHeld.indexOf(key)
+    if (index !== -1) {
+        keysHeld.splice(index, 1)
+        return true
+    }
+    return false
 }
 
-function renderSprite(x, y, rotation, sheet) {
+var frame = 0
+funcs.render = function () {
+    requestAnimationFrame(funcs.render)
+}
+funcs.update = function () {
+    renderSprite
+    setTimeout(funcs.update, frame % 3 === 0 ? 34 : 33)
+}
+
+function renderSprite(name, sheet, rotation) {
+    const s = game.sheets[sheetName]
+    const index = s.ids.indexOf(name)
     ctx.save()
     ctx.translate(x, y)
     if (rotation !== 0) ctx.rotate(rotation)
-    const sw = sheet.spriteW, sh = sheet.spriteH
-    const cols = sheet.imageW / sw
+    const sw = s.spriteW, sh = s.spriteH
+    const cols = s.imageW / sw
     const sx = (index % cols) * sw
     const sy = Math.floor(index / cols) * sh
     const dw = sw
     const dh = sh
     ctx.imageSmoothingEnabled = false
-    ctx.drawImage(sheet.image, sx, sy, sw, sh, -dw * 0.5, -dh * 0.5, dw, dh) // TODO
+    ctx.drawImage(s.image, sx, sy, sw, sh, 0, 0, dw, dh)
     ctx.restore()
 }
 
@@ -78,3 +108,5 @@ document.addEventListener("blur", e => {
     game.mouse = false
     game.keysHeld.length = 0
 })
+
+update()
