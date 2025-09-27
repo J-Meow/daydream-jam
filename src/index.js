82c940974422505cf126273647d4dcf3f3486d0c
plasma4 <72562754+plasma4@users.noreply.github.com>
2025-09-27 11:31:05 -0500

    Added CSS and some boilerplate for js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
-function G() {
-
+"use strict"
+const canvas = document.getElementsByTagName("canvas")[0]
+const ctx = canvas.getContext("2d")
+const game = {
+    keysHeld: [],
+    mouse: false, // or [x, y]
+    data: {},
+    funcs: {},
+    canvas: canvas,
+    ctx: ctx,
+    consts: {
+        DOWN: 0,
+        UP: 1
+    }
 }
-// G.prototype...
+
+var data = game.data
+var funcs = game.funcs
+const C = game.consts
+funcs.handleKey = function (type, { key, repeat, shiftKey, metaKey, ctrlKey }) {
+    var keysHeld = data.keysHeld
+    var keyData = { key, repeat, shift: shiftKey, ctrl: ctrlKey || metaKey }
+    if (type === C.DOWN) {
+        if (keyData.repeat) {
+            // ignore
+        } else {
+            // do stuff...
+            keysHeld = keysHeld.push(key)
+            keys
+        }
+    } else if (type === C.UP) {
+        keyData
+    }
+}
+
+document.addEventListener("resize", game.handleResize)
+document.addEventListener("click", game.handleClick)
+document.addEventListener("keydown", e => game.handleKey(e, C.DOWN))
+document.addEventListener("keyup", e => game.handleKey(e, C.UP))
+document.addEventListener("blur", e => {
+    game.mouse = false
+    game.keysHeld.length = 0
+})
