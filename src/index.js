06fef71232126b8fa579a545d3566f4bdfb6d9c7
swaritnarang <narangswarit02@gmail.com>
2025-09-28 00:15:58 -0500

    Various SFX changes

--- src/index.js
+++ src/index.js
@@ -378,6 +378,7 @@ F.itemInteraction = function (item) {
                 F.heldKey("W") ||
                 F.heldKey(" "))
         ) {
+            document.getElementById("jumpSFX").play()
             player.yVelocity = -0.3 // jump
         }
 
@@ -625,10 +626,9 @@ F.update = function () {
 
 // Add events to buttons
 !(function () {
-    let clickSFX = new Audio("static/assets/sfx/click.wav")
-    clickSFX.volume = 0.5
+    let clickSFX = document.getElementById("clickSFX");
     Array.from(document.getElementsByTagName("button")).forEach((button) => {
-        button.onclick = () => clickSFX.cloneNode().play()
+        button.onclick = () => clickSFX.play()
     })
 
     document.addEventListener("DOMContentLoaded", function () {
